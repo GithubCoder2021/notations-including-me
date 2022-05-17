fb9c2c16deed0cac40934850a6728349aeb7a8ec
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2022-05-17 07:36:39 -0400

    Delete dNotation.ts

--- dNotation.ts
+++
@@ -1,9 +0,0 @@
-class DNotation extends ADNotations.Notation {
-  get name() {
-    return "Simple";
-  }
-
-  formatDecimal(value, places) {
-    return `Mantissa: ${value.mantissa.toFixed(places)},d,  Exponent: ${value.exponent}`;
-  }
-}
